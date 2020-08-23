5f5ee0cec6cff437c0bc11b79274bc83e470a2a4
michaelmutert <github@michaelmutert.com>
2020-08-23 11:03:59 +0200

    Remove default notifications fragment

--- app/src/main/java/com/mmutert/trackmydebt/MainActivity.kt
+++ app/src/main/java/com/mmutert/trackmydebt/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.mmutert.trackmydebt
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_recent_activity, R.id.navigation_notifications))
+            setOf(R.id.navigation_home, R.id.navigation_recent_activity)
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
