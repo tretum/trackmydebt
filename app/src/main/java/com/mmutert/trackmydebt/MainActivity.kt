225aa84e3aaf5c10f808ec12202ba85bbd69d41e
michaelmutert <github@michaelmutert.com>
2020-08-24 11:17:38 +0200

    Use AppBarLayout with Material toolbar, switch to FragmentContainerView

--- app/src/main/java/com/mmutert/trackmydebt/MainActivity.kt
+++ app/src/main/java/com/mmutert/trackmydebt/MainActivity.kt
@@ -2,26 +2,44 @@ package com.mmutert.trackmydebt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAppBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        val navController = navHostFragment!!.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        mAppBarConfiguration = AppBarConfiguration(
             setOf(R.id.navigation_home, R.id.navigation_recent_activity)
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        setupActionBarWithNavController(navController, mAppBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        return (NavigationUI.navigateUp(
+            navController, mAppBarConfiguration
+        ) || super.onSupportNavigateUp())
     }
 }
